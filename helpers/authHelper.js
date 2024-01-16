cd8369b048f362b99d9ed5b2073020a6c6d8a8cd
Arpit Pareek <arpitpareek051@gmail.com>
2024-01-16 12:04:15 +0530

    Finally uploadded the backkend hurrrrrrrrr

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,22 +1,9 @@
-const bcrypt = require("bcrypt");
 
-const hashPassword = async (password) => {
-  try {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    return hashedPassword;
-  } catch (error) {
-    console.log(error);
-  }
-};
 function generateOTP() {
   // Implement your OTP generation logic (e.g., random 6-digit number)
   return Math.floor(100000 + Math.random() * 900000).toString();
 }
 
-const comparePassword = async (password, hashedPassword) => {
-  return bcrypt.compare(password, hashedPassword);
-};
 
 const productsData = [
   {
@@ -141,4 +128,5 @@ const newsData = [
     category: "Announcement - Any random text here.",
   },
 ];
-module.exports = { comparePassword, hashPassword, generateOTP, productsData, newsData };
+
+module.exports = { generateOTP, productsData, newsData };
